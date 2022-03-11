ecf3d9995ecac503431198d899173542821c6727
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-03-11 18:16:35 -0300

    projeto final

--- App.js
+++ App.js
@@ -1,81 +1,136 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
-import { TextInput } from 'react-native-web';
-import cachorrinhos from './src/assets/cachorrinhos.jpeg';
-import gatinhos from './src/assets/gatinhos.jpg';
+import { StatusBar } from 'expo-status-bar';
+import { React, useState } from 'react';
+import {StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
+
+export default function App() {
+  const [valor1, setValor1] = useState();
+  const [valor2, setValor2] = useState();
+  const [resultado, setResultado] = useState(0);
+
+  function somar(){
+    setResultado(parseFloat(valor1) + parseFloat(valor2));
+  }
+
+  function subtrair(){
+    setResultado(parseFloat(valor1) - parseFloat(valor2));
+  }
+
+  function multiplicar(){
+    setResultado(parseFloat(valor1) * parseFloat(valor2));
+  }
+
+  function dividir(){
+    setResultado(parseFloat(valor1) / parseFloat(valor2));
+  }
+
+  return (
+    <View style={styles.container}>
+
+      <Text style={styles.texto}>Calculadora</Text>
+      <View style={styles.bloco}>
+        <Text style={styles.textBlock}>Valor 1</Text>
+        <TextInput style={styles.input}
+        keyboardType="numeric"
+        value={valor1}
+        onChangeText={(texto)=>setValor1(texto)}
+        />
+    </View>
+
+    <View style={styles.bloco}>
+        <Text style={styles.textBlock}>Valor 2</Text>
+        <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={valor2}
+        onChangeText={(texto)=>setValor2(texto)}
+        />
+      </View>
+      <View style={styles.bloco}>
+        <TouchableOpacity 
+        style = {styles.button}
+            onPress={somar}>
+
+          <Text style={styles.textButton}>Somar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+        style = {styles.button}
+            onPress={subtrair}>
+
+          <Text style={styles.textButton}>Subtrair</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+        style = {styles.button}
+            onPress={dividir}>
+
+          <Text style={styles.textButton}>Dividir</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+        style = {styles.button}
+            onPress={multiplicar}>
+
+          <Text style={styles.textButton}>Multiplicar</Text>
+        </TouchableOpacity>
+        </View>
+
+        <View style={styles.bloco}> 
+          <Text style={styles.textoBloco}> Resultado: {resultado} </Text>
+        </View>
+
+      </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#F2F2F2',
-    fontFamily: 'arial',
-    width: '100%',
-    height: '100%'
   },
-  texto: {
-    color: '#8C4A23',
-    margin: 15,
-    fontSize: 17
-  },
-  imagem: {
-    width: 400,
-    height: 360,
-    borderRadius: 10,
-    marginTop: 15
-   },
-   imagem2: {
-    width: 400,
-    height: 360,
-    borderRadius: 10,
-    marginTop: 15,
-   },
-   input: {
-    borderColor: '#0D0000',
-    borderWidth: 2,
-    borderRadius: 10,
-    padding: 5,
-    width: 250,
-    margin: 15
-   },
-   docachorro: {
-    flexDirection: 'column',
-    marginRight: 30,
-    alignItems: 'center',
-   },
-   dogatinho: {
-     flexDirection: 'column',
-     alignItems: 'center',
-   },
-   outro: {
-     alignItems: 'center'
-   },
-   botao: {
-     backgroundColor: '#5555',
-     borderRadius: 10,
-     padding: 10
-   }
-});
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-    <View style={styles.docachorro}>
-      <Image style={styles.imagem} source={cachorrinhos}/>
-      <Text style={styles.texto}> Cachorrinhos </Text>
-    </View>
-    <View style={styles.dogatinho}>
-      <Image style={styles.imagem2} source={gatinhos}/>
-      <Text style={styles.texto}> Gatinhos </Text>
-    </View>
-    <View style={styles.outro}>
-      <TextInput style={styles.input}/>  
-      <TouchableOpacity style={styles.botao}>
-        <Text>Filhotinhos</Text>
-      </TouchableOpacity>
-    </View> 
-    </View>
-  );
-}
+  textoBloco: {
+  fontSize: 20,
+  marginTop : 10,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  },
+
+  texto:{
+    color: '#00FFF',
+    fontSize: 30,
+    marginTop: 15,
+  }, 
+
+  bloco: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30,
+    width: '100%',
+  },
+  input: {
+    borderColor: '#000',
+    borderWidth: 2,
+    fontSize: 20,
+    width: '80%',
+    color: '#000',
+    padding: 15
+  },
+  textBlock: {
+    color: '#000',
+    fontSize: 20,
+    margin: 10
+  },
+  button: {
+    textAlign: 'center',
+    backgroundColor: '#FDDDDE',
+    width: '80%',
+    borderRadius: 5,
+    padding: 15,
+    margin: 15 },
+  textButton: {
+    color: '#000',
+    fontSize: 20,
+  }
+});
